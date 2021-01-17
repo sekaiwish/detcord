48565c7cedcbd44a7556879ef66e962c9c81f448
wishu <wish@sek.ai>
2021-01-18 06:24:17 +1100

    logout

--- server.py
+++ server.py
@@ -1,9 +1,29 @@
 from flask import Flask, request, jsonify
-from classes import User, Message
-import atexit, pickle, asyncio, websockets, multiprocessing
+import time, uuid, json, pickle, asyncio, multiprocessing, atexit, websockets
 app = Flask(__name__)
 app.config["DEBUG"] = True
 memory, users, sessions = {}, {}, {}
+
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.creation_date = time.time()
+        self.sessions = set()
+        users[username] = self
+    def generate_nonce(self):
+        return uuid.uuid4().hex
+
+class Message:
+    def __init__(self, type: int, author, content):
+        self.type = type
+        self.author = author
+        self.content = content
+        self.timestamp = time.time()
+        self.nonce = self.generate_nonce()
+    def generate_nonce(self):
+        return uuid.uuid4().hex
 
 try:
     with open('detcord.dat', 'rb') as fp:
@@ -23,21 +43,36 @@ atexit.register(save)
 async def socket_connection(socket, path):
     print(" * WebSocket connection made")
     while True:
-        name = await socket.recv()
-        await socket.send(f"return:{name}")
+        ws_data = await socket.recv()
+        try:
+            ws_data = json.loads(ws_data)
+            print(ws_data['key'])
+        except:
+            print("Invalid JSON structure")
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    print(users)
     if request.data:
         username = request.json['username']
         password = request.json['password']
         if username in users:
             if users[username].password == password:
                 nonce = users[username].generate_nonce()
+                users[username].sessions.add(nonce)
                 sessions[nonce] = username
-                return jsonify(nonce)
+                return jsonify({"token":nonce})
     return jsonify(0)
+
+@app.route('/api/<token>/logout')
+def logout(token):
+    if token in sessions:
+        user = sessions[token]
+    else:
+        return jsonify(0)
+    for session in users[user].sessions:
+        del sessions[session]
+    users[user].sessions.clear()
+    return jsonify(1)
 
 @app.route('/api/<token>/message/new', methods=['POST'])
 def message_compose(token):
@@ -50,7 +85,6 @@ def message_compose(token):
         memory[message.nonce] = message
         return jsonify(message.nonce)
     return jsonify(0)
-
 
 @app.route('/api/<token>/message/<nonce>/update', methods=['POST'])
 def message_update(token, nonce):
